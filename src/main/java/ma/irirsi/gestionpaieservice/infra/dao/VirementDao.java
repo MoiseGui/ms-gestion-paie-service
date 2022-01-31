fd2a8b28f2b2bf666086f2babfb4561129cf9f96
Moïse Gui <k.moise.gui@gmail.com>
2022-01-31 20:15:34 +0100

    preparation for k8s

--- src/main/java/ma/irirsi/gestionpaieservice/infra/dao/VirementDao.java
+++
@@ -1,7 +0,0 @@
-package ma.irirsi.gestionpaieservice.infra.dao;
-
-import ma.irirsi.gestionpaieservice.infra.entity.VirementEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VirementDao extends JpaRepository<VirementEntity, Long> {
-}
